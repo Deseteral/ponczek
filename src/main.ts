da3b6c5adf5171e3230f107deef6c509bacceaa0
Deseteral <deseteral@gmail.com>
2022-12-30 22:33:43 +0100

    Using alchimia as base

---
+++ src/main.ts
@@ -0,0 +1,45 @@
+/*
+ * TODO: Remove assets from gitignore
+ */
+
+import { Engine } from 'marmolada/engine';
+import { Input } from 'marmolada/input';
+import { preloadSounds } from 'marmolada/sounds';
+import { Textures } from 'marmolada/textures';
+import { MainMenuStage } from 'src/main-menu-stage';
+
+const canvas: HTMLCanvasElement = document.querySelector('canvas')!;
+const context: CanvasRenderingContext2D = canvas.getContext('2d')!;
+
+function tick(): void {
+  context.fillStyle = Engine.secondaryColor;
+  context.fillRect(0, 0, Engine.width, Engine.height);
+  context.fillStyle = Engine.primaryColor;
+  context.strokeStyle = Engine.primaryColor;
+
+  const stage = Engine.activeStage!;
+  stage.update();
+  stage.render(context);
+
+  Input.update();
+
+  if (Engine.shouldCountTicks) Engine.ticks += 1;
+
+  requestAnimationFrame(tick);
+}
+
+(async function main(): Promise<void> {
+  canvas.width = Engine.width;
+  canvas.height = Engine.height;
+  context.imageSmoothingEnabled = false;
+
+  Input.initialize(canvas);
+
+  preloadSounds();
+  await Textures.loadTextures();
+
+  const initialStage = new MainMenuStage();
+  Engine.changeStage(initialStage);
+
+  tick();
+}());
