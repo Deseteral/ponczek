77af122ede1e133665a4bb82fca4c932bb4ab4f8
Deseteral <deseteral@gmail.com>
2023-01-21 19:40:03 +0100

    Setting starting and target tile in tilemap test scene

--- examples/src/scenes/tilemap-test-scene.ts
+++ examples/src/scenes/tilemap-test-scene.ts
@@ -8,10 +8,14 @@ import { SpriteSheet } from 'ponczek/gfx/spritesheet';
 import { Random } from 'ponczek/math/random';
 import { Camera } from 'ponczek/gfx/camera';
 import { Engine } from 'ponczek/engine';
+import { Color } from 'ponczek/gfx/color';
+import { Vector2 } from 'ponczek/math/vector2';
 
 const random = Random.default;
 
 interface TestTile {
+  x: number,
+  y: number,
   type: 'grass' | 'wall',
   variant: number,
   flip: number,
@@ -38,6 +42,9 @@ export class TilemapTestScene extends Scene {
   private map: TestTilemap;
   private tileset: SpriteSheet;
   private camera: Camera;
+  private pointerInWorld: Vector2;
+  private startingTile: (TestTile | null);
+  private targetTile: (TestTile | null);
 
   constructor() {
     super();
@@ -49,8 +56,8 @@ export class TilemapTestScene extends Scene {
     for (let y = 0; y < size; y += 1) {
       for (let x = 0; x < size; x += 1) {
         const tile: TestTile = ((x === 4 && (y < 3 || y > 5)) || (x === 12 && (y < 12 || y > 13)))
-          ? { type: 'wall', variant: 0, flip: 0 }
-          : { type: 'grass', variant: random.nextInt(0, 4), flip: random.nextInt(0, 2) };
+          ? { x, y, type: 'wall', variant: 0, flip: 0 }
+          : { x, y, type: 'grass', variant: random.nextInt(0, 4), flip: random.nextInt(0, 2) };
 
         this.map.setTileAt(x, y, tile);
       }
@@ -58,6 +65,10 @@ export class TilemapTestScene extends Scene {
 
     this.camera = new Camera();
     this.camera.position.set(Engine.screen.width >> 1, Engine.screen.height >> 1);
+
+    this.pointerInWorld = Vector2.zero;
+    this.startingTile = null;
+    this.targetTile = null;
   }
 
   update(): void {
@@ -73,6 +84,13 @@ export class TilemapTestScene extends Scene {
     this.camera.position.x += (dx * cameraSpeed);
     this.camera.position.y += (dy * cameraSpeed);
 
+    this.camera.screenToWorld(Input.pointer, this.pointerInWorld);
+    this.targetTile = this.map.getTileAtWorldPositionV(this.pointerInWorld);
+
+    if (Input.getKeyDown('KeyF')) {
+      this.startingTile = this.targetTile;
+    }
+
     if (Input.getButtonDown('b')) SceneManager.popScene();
   }
 
@@ -80,7 +98,17 @@ export class TilemapTestScene extends Scene {
     scr.clearScreen();
 
     this.camera.begin();
-    this.map.draw(0, 0, scr, this.camera.viewport);
+    {
+      this.map.draw(0, 0, scr, this.camera.viewport);
+
+      if (this.startingTile) {
+        scr.drawSprite(this.tileset.getSpriteAt(0, 2), this.startingTile.x * this.map.size, this.startingTile.y * this.map.size);
+      }
+    }
     this.camera.end();
+
+    if (this.targetTile) {
+      scr.drawText(`Target tile: <${this.targetTile.x}, ${this.targetTile.y}>`, 0, 0, Color.white);
+    }
   }
 }
