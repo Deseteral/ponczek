783bed6fe8b8bab8968d34cec1360283e6483130
Deseteral <deseteral@gmail.com>
2023-01-27 18:07:27 +0100

    Replace color effect docs

--- src/effects/replace-color-effect.ts
+++ src/effects/replace-color-effect.ts
@@ -1,17 +1,34 @@
 import { Color } from 'ponczek/gfx/color';
 import { Effect } from 'ponczek/gfx/effect';
 
+/**
+ * Effect for replacing single color with another color.
+ */
 export class ReplaceColorEffect extends Effect {
   // TODO: For performance - add support for mulitple color replacement
+
+  /**
+   * Color that will be replaced.
+   */
   public sourceColor: Color;
+
+  /**
+   * Color that will be used as replacement.
+   */
   public targetColor: Color;
 
+  /**
+   * Effect setup with color that will be replaced, and color that will be used as replacement.
+   */
   constructor(sourceColor: Color, targetColor: Color) {
     super();
     this.sourceColor = sourceColor;
     this.targetColor = targetColor;
   }
 
+  /**
+   * Effect setup with color that will be replaced, and color that will be used as replacement.
+   */
   public set(sourceColor: Color, targetColor: Color): void {
     this.sourceColor = sourceColor;
     this.targetColor = targetColor;
