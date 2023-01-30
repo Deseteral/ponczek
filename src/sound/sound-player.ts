3143526ac1c24e309080f0dfbb74a234de61f3a1
Deseteral <deseteral@gmail.com>
2023-01-30 20:02:57 +0100

    SoundPlayer docs

--- src/sound/sound-player.ts
+++ src/sound/sound-player.ts
@@ -3,10 +3,17 @@ import { Ponczek } from 'ponczek/ponczek';
 
 export type SoundPlaybackId = number;
 
+/**
+ * Singleton class used to play audio assets.
+ */
 export abstract class SoundPlayer {
   private static idToName: Map<SoundPlaybackId, string> = new Map();
 
-  static playSound(name: string, loop: boolean = false): SoundPlaybackId {
+  /**
+   * Plays sound of given name, that can be optionaly looped.
+   * Returns unique id representing this specific playback (to be used e.g. for stopping).
+   */
+  public static playSound(name: string, loop: boolean = false): SoundPlaybackId {
     const { howl } = Assets.sound(name);
     const id = howl.play();
     howl.loop(loop, id);
@@ -15,7 +22,10 @@ export abstract class SoundPlayer {
     return id;
   }
 
-  static stopSound(id: SoundPlaybackId): void {
+  /**
+   * Stops sound playback represented by the playback id.
+   */
+  public static stopSound(id: SoundPlaybackId): void {
     const name = this.idToName.get(id);
 
     if (!name) {
