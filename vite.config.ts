3f2792fdcc5e5c2d9eea15c23ce27b9561cebf13
Deseteral <deseteral@gmail.com>
2023-02-04 01:05:29 +0100

    Fix base path for examples

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 import path from 'path';
 
 export default defineConfig({
+  base: './',
   root: path.resolve(__dirname, './examples/src'),
   publicDir: path.resolve(__dirname, './examples/assets'),
   build: {
